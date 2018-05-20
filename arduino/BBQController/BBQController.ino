dabd994e3550dc38c1f22331e90325736e9bf3e6
Alexander Buharsky <abuharsky@gmail.com>
2018-05-20 19:55:10 +0300

    thermistor test done

--- arduino/BBQController/BBQController.ino
+++ arduino/BBQController/BBQController.ino
@@ -26,17 +26,12 @@ SmoothServo smoothServo(servo, SERVO_PIN);
 DCMotor motor(MOTOR_PIN);
 Damper damper(smoothServo, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
 MAX6675 thermocouple(THERMO_CLK, THERMO_CS, THERMO_S0);
-ThermoProbe probe0(PROBE0_PIN, 100000, 3950);
-ThermoProbe probe1(PROBE1_PIN, 200000, 3950);
-ThermoProbe probe2(PROBE1_PIN, 200000, 4080);
-ThermoProbe probe3(PROBE1_PIN, 200000, 4120);
-ThermoProbe probe4(PROBE1_PIN, 200000, 4220);
+ThermoProbe probe0(PROBE0_PIN, 200000, 4220);
+ThermoProbe probe1(PROBE1_PIN, 200000, 4220);
 
 TemperatureController controller(damper, motor, thermocouple, probe0, probe1);
 
 SerialCommand sCmd;
-
-float lastTemp = 0.0;
 
 void setup()
 {
@@ -46,9 +41,6 @@ void setup()
   controller.setup();
   probe0.setup();
   probe1.setup();
-  probe2.setup();
-  probe3.setup();
-  probe4.setup();
 
   // Setup callbacks for SerialCommand commands
   sCmd.addCommand("servo", servoControl);
@@ -68,29 +60,7 @@ void loop()
   controller.loop();
   probe0.loop();
   probe1.loop();
-  probe2.loop();
-  probe3.loop();
-  probe4.loop();
   sCmd.readSerial();
-
-  float temp = probe0.readCelsius();
-  if (temp - lastTemp > 0.1) {
-     lastTemp = temp;
-     Serial.print(temp);
-     Serial.print(", ");
-     Serial.print(probe0.readADC());
-     Serial.print(", ");
-     Serial.print(probe1.readCelsius());
-     Serial.print(", ");
-     Serial.print(probe1.readADC());  
-     Serial.print(", ");
-     Serial.print(probe2.readCelsius());
-     Serial.print(", ");
-     Serial.print(probe3.readCelsius());
-     Serial.print(", ");
-     Serial.print(probe4.readCelsius());
-     Serial.println();
-  }
 }
 
 //---------------------------------------------
